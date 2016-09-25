33742dc7b6d688c7d8c83e4ee6146ee23f47ef70
Alex Taylor <alexmctaylorpants@gmail.com>
2016-09-25 13:32:23 -0700

    Working 3-level BSP - time to test and then refactor

--- svg_test.rb
+++ svg_test.rb
@@ -3,23 +3,33 @@ require 'logger'
 require './bsp'
 
 logger = Logger.new(STDOUT)
-logger.level = Logger::DEBUG
+logger.level = Logger::INFO
 
-node_tree = Node.new().split
-logger.debug("Original node children: #{node_tree.children}")
 
-node_tree.children[0].split
-logger.debug("children[0] children: #{node_tree.children[0].children}")
+50.times do |d|
+  node_tree = Node.new().split
+  logger.debug("Original node children: #{node_tree.children}")
 
-node_tree.children[1].split
-logger.debug("children[1] children: #{node_tree.children[1].children}")
+  node_tree.children[0].split
+  logger.debug("children[0] children: #{node_tree.children[0].children}")
 
-File.open("test.svg", "w") do |f|
-  Rasem::SVGImage.new({:width => node_tree.width, :height => node_tree.height}, f) do
-    node_tree.children.each do |outer|
-      outer.children.each do |n|
-        p "rectangle(#{n.origin.x}, #{n.origin.y}, #{n.width}, #{n.height}, stroke: 'black', stroke_width: '2', fill: 'white')"
-        rectangle(n.origin.x, n.origin.y, n.width, n.height, stroke: 'black', stroke_width: '2', fill: 'white')
+  node_tree.children[1].split
+  logger.debug("children[1] children: #{node_tree.children[1].children}")
+
+  node_tree.children[0].children[0].split
+  node_tree.children[0].children[1].split
+  node_tree.children[1].children[0].split
+  node_tree.children[1].children[1].split
+
+  File.open("3_level_bsp_#{d}.svg", "w") do |f|
+    Rasem::SVGImage.new({:width => node_tree.width, :height => node_tree.height}, f) do
+      node_tree.children.each do |outer|
+        outer.children.each do |inner|
+          inner.children.each do |n|
+            p "rectangle(#{n.origin.x}, #{n.origin.y}, #{n.width}, #{n.height}, stroke: 'black', stroke_width: '2', fill: 'white')"
+            rectangle(n.origin.x, n.origin.y, n.width, n.height, stroke: 'black', stroke_width: '2', fill: 'white')
+          end
+        end
       end
     end
   end

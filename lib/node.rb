3310e41e8a60978f5bbc2083ca6812907a9862e3
Alex Taylor <alexmctaylorpants@gmail.com>
2016-09-27 21:29:59 -0700

    Refactor

--- lib/node.rb
+++ lib/node.rb
@@ -14,32 +14,36 @@ class Node
     @children = []
   end
 
-  def next_rand(max = 2)
-    @gen ||= Random.new(seed)
-    @gen.rand(max)
-  end
-
-  def split()
-    direction = next_rand
-
-    if direction == 0 # split horizontally
-      splitWidth = width
-      splitHeight = height / 2
-
-      self.children[0] = Node.new(origin: origin, width: splitWidth, height: splitHeight)
-      self.children[1] = Node.new(origin: Point.new(origin.x, origin.y + splitHeight), width: width, height: height - splitHeight)
-    else # split vertically
-      splitWidth = width / 2
-      splitHeight = height
-
-      self.children[0] = Node.new(origin: origin, width: splitWidth, height: splitHeight)
-      self.children[1] =  Node.new(origin: Point.new(origin.x + splitWidth, origin.y), width: width - splitWidth, height: height)
-    end
-
-    self
-  end
-
   def inspect
     "<Node origin=(#{origin.x}, #{origin.y}), width=#{width}, height=#{height}>"
   end
+
+  def split
+    random_boolean ? split_horizontally : split_vertically
+    self
+  end
+
+  private
+
+  def random_boolean(max = 2)
+    @gen ||= Random.new(seed)
+    @gen.rand(max) == 1
+  end
+
+  def split_horizontally
+    splitWidth = width
+    splitHeight = height / 2
+
+    self.children[0] = Node.new(origin: origin, width: splitWidth, height: splitHeight)
+    self.children[1] = Node.new(origin: Point.new(origin.x, origin.y + splitHeight), width: width, height: height - splitHeight)
+  end
+
+  def split_vertically
+    splitWidth = width / 2
+    splitHeight = height
+
+    self.children[0] = Node.new(origin: origin, width: splitWidth, height: splitHeight)
+    self.children[1] =  Node.new(origin: Point.new(origin.x + splitWidth, origin.y), width: width - splitWidth, height: height)
+  end
+
 end
